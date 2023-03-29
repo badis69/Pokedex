0fd9813955d46031bcab95bf957c2055bfad66d1
Badis Beloucif <beloucifbadis@gmail.com>
2023-03-29 11:28:38 +0200

    quete numero 3

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,9 +1,23 @@
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
 function MyPokemonCard() {
+    const pokemon = pokemonList[0]
+    
     return(
 <figure>
-<img src=" https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="bulbasaur"/>
-<figcaption>Bulbasaur</figcaption>
+{pokemon.imgSrc ? <img src={pokemon.imgSrc} alt={pokemon.name}/> : <p>"???"</p>} 
+<figcaption>{pokemon.name}</figcaption>
 </figure>);
 }
 
 export default MyPokemonCard
+
